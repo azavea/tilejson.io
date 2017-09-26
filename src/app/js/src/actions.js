3667ea50532433ce07d0dfe90ab9f249d7d0275d
Shreshth Khilani <shreshth.khilani@gmail.com>
2017-09-26 00:13:15 -0400

    Integrate Redux Redux isn't only the standard used for state management on the civic-apps-team, it's also an easier way to manage state

--- src/app/js/src/actions.js
+++ src/app/js/src/actions.js
@@ -1,5 +1,39 @@
-export const INC = 'INC';
+export const CHANGE_LAYER_NAME = 'CHANGE_LAYER_NAME';
+export const CHANGE_LAYER_URL = 'CHANGE_LAYER_URL';
+export const CHANGE_TILE_JSON = 'CHANGE_TILE_JSON';
+export const CLEAR_SCREEN = 'CLEAR_SCREEN';
+export const CHANGE_SHARE_LINK = 'CHANGE_SHARE_LINK';
 
-export function inc() {
-    return { type: INC };
+export function changeLayerName(payload) {
+    return {
+        type: CHANGE_LAYER_NAME,
+        payload,
+    };
+}
+
+export function changeLayerUrl(payload) {
+    return {
+        type: CHANGE_LAYER_URL,
+        payload,
+    };
+}
+
+export function changeTileJson(payload) {
+    return {
+        type: CHANGE_TILE_JSON,
+        payload,
+    };
+}
+
+export function clearScreen() {
+    return {
+        type: CLEAR_SCREEN,
+    };
+}
+
+export function changeShareLink(payload) {
+    return {
+        type: CHANGE_SHARE_LINK,
+        payload,
+    };
 }
--- src/app/js/src/reducers.js
+++ src/app/js/src/reducers.js
@@ -1,16 +1,51 @@
 import { combineReducers } from 'redux';
 
-import { INC } from './actions';
+import {
+    CHANGE_LAYER_NAME,
+    CHANGE_LAYER_URL,
+    CHANGE_TILE_JSON,
+    CLEAR_SCREEN,
+    CHANGE_SHARE_LINK,
+} from './actions';
+
+import {
+    getDefaultTileJSON,
+} from './constants';
 
 const initialState = {
-    n: 0,
+    name: '',
+    url: '',
+    tileJSON: getDefaultTileJSON(),
+    tileJSONString: JSON.stringify(getDefaultTileJSON(), null, '\t'),
+    shareLink: '',
 };
 
 function mainReducer(state = initialState, action) {
     switch (action.type) {
-        case INC:
+        case CHANGE_LAYER_NAME:
             return Object.assign({}, state, {
-                n: state.n + 1,
+                name: action.payload.name,
+            });
+        case CHANGE_LAYER_URL:
+            return Object.assign({}, state, {
+                url: action.payload.url,
+            });
+        case CHANGE_TILE_JSON:
+            return Object.assign({}, state, {
+                tileJSON: action.payload.tileJSON,
+                tileJSONString: JSON.stringify(action.payload.tileJSON, null, '\t'),
+            });
+        case CLEAR_SCREEN:
+            return Object.assign({}, state, {
+                tileJSON: getDefaultTileJSON(),
+                tileJSONString: JSON.stringify(getDefaultTileJSON(), null, '\t'),
+                shareLink: '',
+                url: '',
+                name: '',
+            });
+        case CHANGE_SHARE_LINK:
+            return Object.assign({}, state, {
+                shareLink: action.payload.shareLink,
             });
         default:
             return state;
